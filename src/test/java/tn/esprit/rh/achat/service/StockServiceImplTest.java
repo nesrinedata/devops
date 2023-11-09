c8823f6a87d7558946aae97e4abcffe03b208510
HamzaManai <mhamza1506@gmail.com>
2023-11-09 11:02:45 +0100

    last changes2

--- src/test/java/tn/esprit/rh/achat/service/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/service/StockServiceImplTest.java
@@ -27,55 +27,88 @@ class StockServiceImplTest {
     private StockServiceImpl stockService;
 
     // Test methods will go here
-
-    @Test
-    void testDeleteStock()
-        Long stockId = 1L;
-
-        stockService.deleteStock(stockId);
-
-        verify(stockRepository, times(1)).deleteById(stockId);
-    }
-    @Test
-    void testAddStock() {
-        Stock stock = new Stock();
-        when(stockRepository.save(stock)).thenReturn(stock);
-        Stock result = stockService.addStock(stock);
-        assertEquals(stock, result);
-        verify(stockRepository, times(1)).save(stock);
-    }
-
-    @Test
-    void testRetrieveStock() {
-        Long stockId = 1L;
-        Stock stock = new Stock();
-        when(stockRepository.findById(stockId)).thenReturn(Optional.of(stock));
-        Stock result = stockService.retrieveStock(stockId);
-        assertEquals(stock, result);
-        verify(stockRepository, times(1)).findById(stockId);
-    }
     @Test
     void testRetrieveAllStocks() {
         // Arrange
         Stock stock = new Stock();
         List<Stock> stocks = Collections.singletonList(stock);
         when(stockRepository.findAll()).thenReturn(stocks);
+
+        // Act
         List<Stock> result = stockService.retrieveAllStocks();
+
+        // Assert
         assertEquals(stocks, result);
         verify(stockRepository, times(1)).findAll();
     }
 
     @Test
+    void testAddStock() {
+        // Arrange
+        Stock stock = new Stock();
+        when(stockRepository.save(stock)).thenReturn(stock);
+
+        // Act
+        Stock result = stockService.addStock(stock);
+
+        // Assert
+        assertEquals(stock, result);
+        verify(stockRepository, times(1)).save(stock);
+    }
+
+    @Test
+    void testRetrieveStock() {
+        // Arrange
+        Long stockId = 1L;
+        Stock stock = new Stock();
+        when(stockRepository.findById(stockId)).thenReturn(Optional.of(stock));
+
+        // Act
+        Stock result = stockService.retrieveStock(stockId);
+
+        // Assert
+        assertEquals(stock, result);
+        verify(stockRepository, times(1)).findById(stockId);
+    }
+
+    @Test
     void testRetrieveStatusStock() {
+        // Arrange
         Stock stock = new Stock();
         List<Stock> stocksEnRouge = Collections.singletonList(stock);
         when(stockRepository.retrieveStatusStock()).thenReturn(stocksEnRouge);
+
+        // Act
         String result = stockService.retrieveStatusStock();
+
+        // Assert
         assertNotNull(result);
         verify(stockRepository, times(1)).retrieveStatusStock();
     }
 
+    @Test
+    void testUpdateStock() {
+        // Arrange
+        Stock stock = new Stock();
+        when(stockRepository.save(stock)).thenReturn(stock);
 
+        // Act
+        Stock result = stockService.updateStock(stock);
 
+        // Assert
+        assertEquals(stock, result);
+        verify(stockRepository, times(1)).save(stock);
+    }
 
+    @Test
+    void testDeleteStock() {
+        // Arrange
+        Long stockId = 1L;
+
+        // Act
+        stockService.deleteStock(stockId);
+
+        // Assert
+        verify(stockRepository, times(1)).deleteById(stockId);
+    }
 }
